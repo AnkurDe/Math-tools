db8ce3b57e9460c5032d84f248fcf5c94ca45054
Ankur De <ankurde2005@gmail.com>
2025-06-06 15:23:31 +0530

    Added Gram Schmidt Orthogonalization

---
+++ src/Matrix/GramSchmidtOrthogonalization.java
@@ -0,0 +1,33 @@
+package Matrix;
+
+import static Matrix.Transpose.transpose;
+import static Vector.Subtraction.subtract;
+import static Vector.Dot.dot;
+import static Vector.Scale.scale;
+
+public class GramSchmidtOrthogonalization {
+    // Project v on u
+    public static double[] proj(double[] v, double[] u) {
+        return scale(u, dot(v,u)/dot(u, u));
+    }
+
+    public static double[][] GSO(double[][] matrix) {
+        if (matrix[0].length == 1) {
+            return matrix.clone();
+        }
+
+        double[][] orig = transpose(matrix);
+        double[][] ortho = new double[orig.length][orig[0].length];
+
+        // There are orig.length number of vectors
+        for (int i = 0; i < orig.length; i++) {
+            double[] vec = orig[i];
+
+            for (int j = 0; j < i; j++) {
+                vec = subtract(vec, proj(orig[i], ortho[j]));
+            }
+            ortho[i] = vec;
+        }
+        return transpose(ortho);
+    }
+}
