c7db89beefb3887855e570858e36616cffd38b14
Ankur De <ankurde2005@gmail.com>
2025-06-06 11:44:52 +0530

    Changed logic of taking product of diagonal elements

--- src/Matrix/DiagonalProduct.java
+++ src/Matrix/DiagonalProduct.java
@@ -2,10 +2,10 @@ package Matrix;
 
 // For taking diagonal products of elements
 final public class DiagonalProduct {
-    public static double diag_prod(double[][] Matrix) {
+    public static double diag_prod(double[][] matrix) {
         double dp = 1;
-        for (int i = 0; i < Matrix.length; i++) {
-            dp *= Matrix[i][i];
+        for (int i = 0; i < matrix.length && i < matrix[0].length; i++) {
+            dp *= matrix[i][i];
         }
         return dp;
     }
