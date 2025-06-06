08e8f4120bf0cf808466687d0bc97506c6e94dd1
Ankur De <ankurde2005@gmail.com>
2025-06-06 16:55:10 +0530

    minor

--- src/Matrix/PrintMatrix.java
+++ src/Matrix/PrintMatrix.java
@@ -7,7 +7,7 @@ final public class PrintMatrix {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print("  " + doubles[j]);
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
