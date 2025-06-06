be466d10e13142a72531a9d464c20ce570e69dc7
Ankur De <ankurde2005@gmail.com>
2025-06-06 16:16:06 +0530

    name change

--- src/Matrix/GramSchmidtOrthonormalization.java
+++ src/Matrix/GramSchmidtOrthonormalization.java
@@ -7,7 +7,7 @@ import static Matrix.Transpose.transpose;
 import static Vector.Normalize.normalise;
 
 public class GramSchmidtOrthonormalization {
-    public static double[][] GSOrthonormalization(double[][] matrix){
+    public static double[][] GSOrthonormalization(double[][] matrix) {
         double[][] A = GSOt(matrix);
         IntStream.range(0, A.length).forEach(i -> A[i] = normalise(A[i]));
         return transpose(A);
