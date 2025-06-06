5278eadc2842a2f01291ba48f3c328244a502e94
Ankur De <ankurde2005@gmail.com>
2025-06-06 11:39:39 +0530

    Added feature for generating identity matrices

--- src/Matrix/Identity.java
+++ src/Matrix/Identity.java
@@ -1,3 +1,21 @@
 package Matrix;
 
-final public class Identity{}
+final public class Identity{
+    // This class is a placeholder for generating Identify matrix
+
+    public static double[][] eye(double[][] matrix) {
+        return eye(matrix.length, matrix[0].length);
+    }
+
+    public static double[][] eye(int rows, int cols) {
+        double[][] identity = new double[rows][cols];
+        for (int i = 0; i < rows && i < cols; i++) {
+            identity[i][i] = 1;
+        }
+        return identity;
+    }
+
+    public static double[][] eye(int n) {
+        return eye(n, n);
+    }
+}
