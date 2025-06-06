b4fe71f77b1315ad4d0af8867f161ac61b608f0b
Ankur De <ankurde2005@gmail.com>
2025-06-06 15:55:16 +0530

    Calculating Determinant

--- src/Matrix/Determinant.java
+++ src/Matrix/Determinant.java
@@ -1,5 +1,13 @@
 package Matrix;
 
+import static Matrix.DiagonalProduct.diag_prod;
+import static Matrix.Matrix.lu;
+
 final public class Determinant {
 
+    // To calculate the determinant of a matrix by LU decomposition
+    public static double det(double[][] matrix) {
+        return diag_prod(lu(matrix)[1]);// Perform LU Decomposition
+        // and get the diagonal product of Upper triangular matrix from LU Decomposition
+    }
 }
