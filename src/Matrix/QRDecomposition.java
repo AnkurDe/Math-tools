6f9f28ed3849fa9b50c6b71ae02404bc3eac993c
Ankur De <ankurde2005@gmail.com>
2025-06-07 20:25:07 +0530

    changed function names

--- src/Matrix/QRDecomposition.java
+++ src/Matrix/QRDecomposition.java
@@ -7,7 +7,7 @@ import static Matrix.Transpose.transpose;
 
 public class QRDecomposition {
     public static double[][][] qr(double[][] matrix){
-        double[][] Q = GSOrthonormalization(matrix);
+        double[][] Q = GSO(matrix);
         double[][] R = multiply(transpose(Q), matrix);
         return new double[][][]{Q, R};
     }
