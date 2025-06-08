20201acb147fd3a0a27764a4caa45a5b1b15525a
Ankur De <ankurde2005@gmail.com>
2025-06-08 19:31:06 +0530

    imports handling for decompositions

--- src/Matrix/Inverse.java
+++ src/Matrix/Inverse.java
@@ -1,5 +1,11 @@
 package Matrix;
 
+import static Matrix.Augment.aug;
+import static Matrix.Identity.eye;
+import static Matrix.PrintMatrix.printMat;
+import static Vector.Scale.scale;
+import static Vector.Subtraction.subtract;
+
 final public class Inverse
 {
     // This class is a placeholder for the Inverse matrix operation.
