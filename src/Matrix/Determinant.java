55dc59c128a1138f8483764eb2f7805977565da4
Ankur De <ankurde2005@gmail.com>
2025-06-09 11:07:46 +0530

    Better error handling

--- src/Matrix/Determinant.java
+++ src/Matrix/Determinant.java
@@ -1,12 +1,16 @@
 package Matrix;
 
 import static Matrix.DiagonalProduct.diag_prod;
-import static Matrix.Matrix.lu;
+import static Matrix.Decomposition.lu;
+import static Matrix.IsSquare.isSquare;
 
 final public class Determinant {
 
     // To calculate the determinant of a matrix by LU decomposition
     public static double det(double[][] matrix) {
+        if (!isSquare(matrix)){
+            throw new MatrixError("Not a square matrix");
+        }
         return diag_prod(lu(matrix)[1]);// Perform LU Decomposition
         // and get the diagonal product of Upper triangular matrix from LU Decomposition
     }
