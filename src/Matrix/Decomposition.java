c143c429d0c680ae6f65fd1ab97d65dabf9ad5ff
Ankur De <ankurde2005@gmail.com>
2025-06-09 11:05:21 +0530

    Moved LU Decomposition from LUdecomposition.java

--- src/Matrix/Decomposition.java
+++ src/Matrix/Decomposition.java
@@ -16,6 +16,34 @@ public class Decomposition {
         return new double[][][]{Q, R};
     }
 
+    public static double[][][] lu(double[][] A) {
+        int n = A.length;
+        double[][] L = new double[n][n];
+        double[][] U = new double[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    L[i][j] = 1.0;
+                } else {
+                    L[i][j] = 0.0;
+                }
+                U[i][j] = A[i][j];
+            }
+        }
+
+        for (int k = 0; k < n - 1; k++) {
+            for (int i = k + 1; i < n; i++) {
+                L[i][k] = U[i][k] / U[k][k];
+                for (int j = k; j < n; j++) {
+                    U[i][j] -= L[i][k] * U[k][j];
+                }
+            }
+        }
+
+        return new double[][][]{L, U};
+    }
+
 //    private static void QRTest() {
 //        // Test 1: 2x2 identity matrix
 //        double[][] matrix1 = {
