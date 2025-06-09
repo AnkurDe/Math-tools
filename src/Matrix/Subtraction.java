f91b5a1b10f04be8cc49f78e3b85287cb2ac0559
Ankur De <ankurde2005@gmail.com>
2025-06-09 11:00:50 +0530

    Better exception handling

--- src/Matrix/Subtraction.java
+++ src/Matrix/Subtraction.java
@@ -4,8 +4,15 @@ import static Matrix.Checker.checker;
 
 // For performing subtraction
 final public class Subtraction {
-    public static double[][] Subtr(double[][] A, double[][] B) {
-        checker(A, B);
+    public static double[][] sub(double[][] A, double[][] B) {
+
+        if (A.length != B.length) {
+            throw new MatrixCorruptException("Mismatch in number of rows of matrices");
+        }
+        if (A[0].length != B[0].length){
+            throw new MatrixCorruptException("Mismatch in number of columns of matrices");
+        }
+
         double[][] Subtract = new double[A.length][A.length];
         for (int i = 0; i < A.length; i++)
             for (int j = 0; j < A.length; j++)
