e811d4a6169964e30d5f593a6bd86763ab40ceb4
Ankur De <ankurde2005@gmail.com>
2025-06-16 01:12:52 +0530

    Changed some function definitions

--- src/Calculus/SVC.java
+++ src/Calculus/SVC.java
@@ -17,7 +17,7 @@ final public class SVC {
     // y0 is initial value
     // t is final time
     // fn is the function to be solved
-    public final static double RK4(double t0, double y0, double t, FunctionMultipleVariable fn) {
+    public static double RK4(double t0, final double y0, final double t, final FunctionMultipleVariable fn) {
         double y = y0;
         while (t0 < t) {
             double k1 = fn.function(t0, y);
@@ -36,7 +36,7 @@ final public class SVC {
     // y0 is initial value
     // t is final time
     // fn is the function to be solved
-    public final static double RK5(double t0, double y0, double t, FunctionMultipleVariable fn) {
+    public static double RK5(double t0, final double y0, final double t, final FunctionMultipleVariable fn) {
         double step = stepSize;
         int n = (int) ((t - t0) / step);
 
@@ -71,7 +71,7 @@ final public class SVC {
 
     // INCOMPLETE
     // Uses either of RK4 or RK5 method based on accuracy difference
-    public final static double ode45(double t0, double y0, double t, FunctionMultipleVariable fn) {
+    public static double ode45(final double t0, final double y0, double t, final FunctionMultipleVariable fn) {
         // Measure error and calculate tolerance
 
         // Use RK4 and RK5 error is less than tolerance
@@ -85,13 +85,13 @@ final public class SVC {
     }
 
     // COMPLETE
-    public final static double differentiate(final double val, FunctionSingleVariable fn) {
+    public static double differentiate(final double val, final FunctionSingleVariable fn) {
         return (fn.function(val + stepSize) - fn.function(val - stepSize)) / (2.0 * stepSize);
     }
 
 
-    // COMPLETE
-    double differentiate(double val, int order, FunctionSingleVariable fn) {
+    // PROBLEM
+    public static double differentiate(final double val, int order, final FunctionSingleVariable fn) {
         if (order <= 0) {
             throw new IllegalArgumentException("The order is less than or equal to 0");
         } else if (order == 1) {
@@ -116,11 +116,11 @@ final public class SVC {
     }
 
     // INCOMPLETE
-    double df(final double valX, final double valY, final double step, FunctionMultipleVariable fn) {
+    public static double df(final double valX, final double valY, final double step, FunctionMultipleVariable fn) {
         return (fn.function(valX - step, valY) + fn.function(valX, valY - step)) * (step * 2.0);
     }
 
-    double integrate(final double start, final double end, FunctionSingleVariable fn) {
+    public static double integrate(final double start, final double end, FunctionSingleVariable fn) {
 
         double ar = 0.0, x1 = start, x2 = start + stepSize;
         while (x2 < end) {
@@ -136,7 +136,7 @@ final public class SVC {
         return ar;
     }
 
-    int factorial(final int num) {
+    public static int factorial(final int num) {
         if (num<0) throw new IllegalArgumentException("The number is less than 0");
         if (num > 20) throw new IllegalArgumentException("The number can cause overflow");
 
