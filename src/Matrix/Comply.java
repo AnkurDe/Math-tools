f9387246d3eeef0a6745d0b01ec951d9a169b525
Ankur De <ankurde2005@gmail.com>
2025-06-15 12:20:36 +0530

    Removed an unnecessary file

--- src/Matrix/Comply.java
+++
@@ -1,8 +0,0 @@
-package Matrix;
-
-final public class Comply {
-    // Checks compatibility for addition, subtraction, individual operation
-    public static boolean compla(double[][] A, double[][] B) {
-        return A.length != B.length || A[0].length != B[0].length;
-    }// Not tested
-}
