69db34b325d489f87143a911ea320e56c6fe9d20
Ankur De <ankurde2005@gmail.com>
2025-06-23 01:01:18 +0530

    Added documentation + removed an unnecessary function

--- src/Calculus/MVC.java
+++ src/Calculus/MVC.java
@@ -2,13 +2,38 @@ package Calculus;
 
 import FunctionDefinations.ScalarOutput.FunctionMultipleVariable;
 
+/**
+ * MVC (Multi Variable Calculus) provides static methods for numerical calculus operations
+ * on functions of several variables, including gradient, Hessian, and mixed partial derivatives.
+ *
+ * <p>
+ * Features:
+ * <ul>
+ *   <li>Numerical gradient (vector of partial derivatives)</li>
+ *   <li>Numerical Hessian (matrix of second derivatives)</li>
+ *   <li>Mixed partial derivatives (cross derivatives)</li>
+ * </ul>
+ * </p>
+ */
 public class MVC {
+    /** Step size for numerical methods */
     public static double stepSize = 0.000001;
+    /** Tolerance for zero detection */
     public static double tolerance = 0.000001;
 
+    /** Mathematical constant pi */
     public static final double pi = 3.14159265358979323846;
+    /** Mathematical constant e */
     public static final double e = 2.718281828459045;
 
+    /**
+     * Computes the gradient vector of a multivariable function at a given point.
+     * Uses a central difference for each variable.
+     * @param fn Function f(x1, x2, ..., xn)
+     * @param x Point at which to compute the gradient
+     * @return Gradient vector (partial derivatives with respect to each variable)
+     * @throws RuntimeException if the function dimension and input dimension do not match
+     */
     public static double[] grad(FunctionMultipleVariable fn, double... x) {
         int n = x.length;
         double[] grad = new double[n];
@@ -25,6 +50,15 @@ public class MVC {
         return grad;
     }
 
+    /**
+     * Computes the second partial derivative with respect to two variables.
+     * Uses central difference for mixed partials and standard second difference for pure partials.
+     * @param fn Function f(x1, x2, ..., xn)
+     * @param var1 Index of the first variable (0-based)
+     * @param var2 Index of the second variable (0-based)
+     * @param x Point at which to compute the derivative
+     * @return Approximate value of d^2f/dx_var1 dx_var2 at x
+     */
     public static double d2f_dx1dx2(final FunctionMultipleVariable fn, final int var1, final int var2, final double... x) {
         if (var1 == var2) {
             double[] t = x.clone();
@@ -64,6 +98,13 @@ public class MVC {
         return (a - b - c + d) / (4.0 * stepSize * stepSize);
     }
 
+    /**
+     * Computes the Hessian matrix (matrix of all second partial derivatives) at a given point.
+     * The Hessian is symmetric for functions with continuous second derivatives.
+     * @param fn Function f(x1, x2, ..., xn)
+     * @param x Point at which to compute the Hessian
+     * @return Hessian matrix (n x n)
+     */
     public static double[][] hessian(FunctionMultipleVariable fn, double... x) {
         double[][] hess = new double[x.length][x.length];
 
